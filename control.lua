bfeba1d204b8994f3f0f32fd8e2d7b65cf54e6f8
heyqule <hq.jfax@gmail.com>
2022-06-19 15:06:14 -0400

    - Added support to set neutral for neutral forces that created from other mods.

--- control.lua
+++ control.lua
@@ -35,6 +35,8 @@ local createRace = function()
     else
         ErmForceHelper.set_friends(game, FORCE_NAME, true)
     end
+
+    ErmForceHelper.set_neutral_force(game, FORCE_NAME)
 end
 
 local addRaceSettings = function()
